0469ec41d4adc708c9dbcbb7e3d585130c7e426f
Zhahrany <zhahrany@gmail.com>
2024-02-03 16:10:35 +0700

    update all service starting message

--- client/pages/index.js
+++ client/pages/index.js
@@ -18,7 +18,7 @@ const LandingPage = ({ currentUser, tickets }) => {
 
   return (
     <div>
-      <h2>Tickets</h2>
+      <h2>Tickets App</h2>
       <table className="table">
         <thead>
           <tr>
