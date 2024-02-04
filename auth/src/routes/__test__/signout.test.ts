5935316452a675a12bb3f445f6925f369cfdb2e0
Zhahrany <zhahrany@gmail.com>
2024-02-04 16:25:51 +0700

    update auth test

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -17,6 +17,6 @@ it("clears the cookie after signing out", async () => {
 
   console.log(response.get("Set-Cookie"));
   expect(response.get("Set-Cookie")[0]).toEqual(
-    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; samesite=none; httponly"
   );
 });
